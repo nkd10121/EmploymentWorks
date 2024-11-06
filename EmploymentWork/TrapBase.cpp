e230acabd7947dadc9a595940fe4d3c9005707e1
nkd10121 <nakada10121@icloud.com>
2024-11-06 18:49:40 +0900

    up

---
+++ EmploymentWork/TrapBase.cpp
@@ -0,0 +1,22 @@
+﻿#include "TrapBase.h"
+
+TrapBase::TrapBase(GameObjectTag tag):
+	ObjectBase(Collidable::Priority::Low, tag)
+{
+}
+
+TrapBase::~TrapBase()
+{
+}
+
+void TrapBase::Init()
+{
+}
+
+void TrapBase::Update()
+{
+}
+
+void TrapBase::Draw()
+{
+}
---
+++ EmploymentWork/TrapBase.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "ObjectBase.h"
+class TrapBase : public ObjectBase
+{
+public:
+	TrapBase(GameObjectTag tag);
+	~TrapBase();
+
+	void Init();
+	void Update();
+	void Draw();
+
+};
+
