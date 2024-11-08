49d5a187cf1d44b153a9f2df125fe020b783fbd8
nkd10121 <nakada10121@icloud.com>
2024-11-08 14:08:40 +0900

    リファクタリング

--- EmploymentWork/SceneDebug.cpp
+++ EmploymentWork/SceneDebug.cpp
@@ -16,20 +16,30 @@ namespace
 		"ScenePause",
 	};
 
-	constexpr int kTextX = 64;
-	constexpr int kTextY = 32;
-	constexpr int kTextYInterval = 16;
+	/*テキスト描画関係*/
+	constexpr int kTextX = 64;			//テキスト描画X座標
+	constexpr int kTextY = 32;			//テキスト描画Y座標
+	constexpr int kTextYInterval = 16;	//テキスト描画Y座標の空白
 }
 
+/// <summary>
+/// コンストラクタ
+/// </summary>
 SceneDebug::SceneDebug() :
 	SceneBase("SCENE_DEBUG")
 {
 }
 
+/// <summary>
+/// デストラクタ
+/// </summary>
 SceneDebug::~SceneDebug()
 {
 }
 
+/// <summary>
+/// //リソースのロード開始
+/// </summary>
 void SceneDebug::StartLoad()
 {
 	//このシーンでロードするべきリソースのパスを取得
@@ -45,56 +55,81 @@ void SceneDebug::StartLoad()
 	SetUseASyncLoadFlag(false);
 }
 
+/// <summary>
+/// リソースのロードが終了したかどうか
+/// </summary>
 bool SceneDebug::IsLoaded() const
 {
 	return true;
 }
 
+/// <summary>
+/// 初期化
+/// </summary>
 void SceneDebug::Init()
 {
+	//最初、タイトルシーンを選択してくる状態にする
 	m_destinationScene = static_cast<eDestination>(static_cast<int>(eDestination::Start) + 1);
 }
 
+/// <summary>
+/// 終了
+/// </summary>
 void SceneDebug::End()
 {
 }
 
+/// <summary>
+/// 更新
+/// </summary>
 void SceneDebug::Update()
 {
+	//上を入力したら
 	if (Input::GetInstance().IsTriggered("UP"))
 	{
+		//現在選択している項目から一個上にずらす
 		m_destinationScene = static_cast<eDestination>(static_cast<int>(m_destinationScene) - 1);
 
+		//もし一番上の項目を選択している状態になっていたら
 		if (m_destinationScene == eDestination::Start)
 		{
+			//一個下にずらす
 			m_destinationScene = static_cast<eDestination>(static_cast<int>(m_destinationScene) + 1);
 		}
 	}
 
+	//下を入力したら
 	if (Input::GetInstance().IsTriggered("DOWN"))
 	{
+		//現在選択している項目から一個下にずらす
 		m_destinationScene = static_cast<eDestination>(static_cast<int>(m_destinationScene) + 1);
 
+		//もし一番下の項目を選択している状態になっていたら
 		if (m_destinationScene == eDestination::Last)
 		{
+			//一個上にずらす
 			m_destinationScene = static_cast<eDestination>(static_cast<int>(m_destinationScene) - 1);
 		}
 	}
 
+	//決定ボタンを押したら現在選択しているシーンに遷移する
 	if (Input::GetInstance().IsTriggered("OK"))
 	{
+		//タイトルシーンに遷移する
 		if (m_destinationScene == eDestination::Title)
 		{
 			SceneManager::GetInstance().ChangeScene(std::make_shared<SceneTitle>());
 			EndThisScene();
 			return;
 		}
+		//ゲームシーンに遷移する
 		else if (m_destinationScene == eDestination::InGame)
 		{
 			SceneManager::GetInstance().ChangeScene(std::make_shared<SceneGame>());
 			EndThisScene();
 			return;
 		}
+		//ポーズシーンを上に表示する
 		else if (m_destinationScene == eDestination::Pause)
 		{
 			SceneManager::GetInstance().PushScene(std::make_shared<ScenePause>());
@@ -103,6 +138,9 @@ void SceneDebug::Update()
 	}
 }
 
+/// <summary>
+/// 描画
+/// </summary>
 void SceneDebug::Draw()
 {
 	DrawString(kTextX - 24, kTextY + kTextYInterval * (m_destinationScene - 1), "→", 0xff0000);
