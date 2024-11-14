b04a457f3781aa45d3d9bff92386c5e49e3cc5f5
nkd10121 <nakada10121@icloud.com>
2024-11-14 10:36:28 +0900

    群れクラスの作成だけ

---
+++ EmploymentWork/SwarmEnemy.cpp
@@ -0,0 +1,24 @@
+﻿#include "SwarmEnemy.h"
+
+SwarmEnemy::SwarmEnemy()
+{
+}
+
+SwarmEnemy::~SwarmEnemy()
+{
+}
+
+void SwarmEnemy::Update()
+{
+}
+
+void SwarmEnemy::Draw()
+{
+}
+
+void SwarmEnemy::AddSwarm(std::list<EnemyBase> add,Vec3 center)
+{
+	for (auto& enemy : add)
+	{
+	}
+}
---
+++ EmploymentWork/SwarmEnemy.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <list>
+
+#include "EnemyBase.h"
+
+/// <summary>
+/// 敵の群れを管理するクラス
+/// </summary>
+class SwarmEnemy
+{
+public:
+	SwarmEnemy();
+	~SwarmEnemy();
+
+	void Update();
+	void Draw();
+
+	void AddSwarm(std::list<EnemyBase> add, Vec3 center);
+
+private:
+	std::list<EnemyBase> m_swarm;	//群れの構成員
+};
+
