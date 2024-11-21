4ea8d7130e35cc99db0f0c2988d42ba243eb02f5
nkd10121 <nakada10121@icloud.com>
2024-11-21 11:34:41 +0900

    ローカル座標クラスの作成

---
+++ EmploymentWork/LocalPos.cpp
@@ -0,0 +1,36 @@
+﻿#include "LocalPos.h"
+
+LocalPos::LocalPos():
+	centerPos(),
+	localPos(),
+	centerRotVec()
+{
+}
+
+Vec3 LocalPos::GetWorldPos()
+{
+	Vec3 ret;
+
+	Vec3 move = localPos;
+
+	MATRIX mat;
+	CreateRotationXYZMatrix(&mat,centerRotVec.x, centerRotVec.y, centerRotVec.z);
+
+	//move = move.MatTransform(mat);
+
+	//ans = move + m_centerPos;
+
+	return ret;
+}
+
+void LocalPos::SetFrontPos(Vec3 frontPos)
+{
+	float vX = centerPos.x - frontPos.x;
+	float vZ = centerPos.z - frontPos.z;
+
+	float yAngle = std::atan2f(vX, vZ);
+
+	Vec3 rot(0.0f, yAngle + DX_PI_F, 0.0f);
+
+	centerRotVec = rot;
+}
