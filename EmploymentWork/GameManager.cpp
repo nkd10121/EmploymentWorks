99c950a38348d6cdd83c7898916bc9ea311ca92a
nkd10121 <nakada10121@icloud.com>
2024-11-28 09:37:54 +0900

    GameManagerの大枠を作成

---
+++ EmploymentWork/GameManager.cpp
@@ -0,0 +1,36 @@
+﻿#include "GameManager.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+GameManager::GameManager()
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+GameManager::~GameManager()
+{
+}
+
+/// <summary>
+/// 初期化
+/// </summary>
+void GameManager::Init()
+{
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void GameManager::Update()
+{
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+void GameManager::Draw()
+{
+}
---
+++ EmploymentWork/GameManager.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+class GameManager
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	GameManager();
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	virtual ~GameManager();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Init();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private:
+
+};
