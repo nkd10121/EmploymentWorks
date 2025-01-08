ffd9321f3074202c6cca14ec470287d84bb587e3
nkd10121 <nakada10121@icloud.com>
2025-01-08 16:37:40 +0900

    Scoreマネージャーの大枠を作成

---
+++ EmploymentWork/ScoreManager.cpp
@@ -0,0 +1,32 @@
+﻿#include "ScoreManager.h"
+
+ScoreManager* ScoreManager::m_instance = nullptr;
+
+ScoreManager::ScoreManager():
+	m_clearTime(0),
+	m_playerKillNum(0),
+	m_trapKillNum(0),
+	m_crystalHp(0),
+	m_maxComboNum(0),
+	m_score(0)
+{
+}
+
+ScoreManager::~ScoreManager()
+{
+}
+
+void ScoreManager::Clear()
+{
+	m_clearTime = 0;
+	m_playerKillNum = 0;
+	m_trapKillNum = 0;
+	m_crystalHp = 0;
+	m_maxComboNum = 0;
+	m_score = 0;
+}
+
+const void ScoreManager::CalculationScore()
+{
+	return void();
+}
